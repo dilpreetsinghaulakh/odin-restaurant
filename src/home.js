7c0ecdea024d7f83202343ac1e57dd34b2c5889d
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-08-16 14:55:51 +0530

    Completed the hero section.

--- src/home.js
+++ src/home.js
@@ -1,39 +1,49 @@
-import heroImage from "./assets/top-view-pizza-home.png"
+import heroImage from "./assets/top-view-pizza-home.png";
 
 function Home() {
   const homePage = document.createElement("div");
   const heroSection = document.createElement("div");
-  heroSection.className = "flex";
+  heroSection.className = "flex gap-8 h-[80vh] flex-col mt-8 md:mt-0 md:flex-row";
 
   const heroLeft = document.createElement("div");
 
   const heroHeading = document.createElement("h1");
   heroHeading.textContent = "Experience the ultimate Cuisine";
-  heroHeading.className = "text-6xl font-serif"
+  heroHeading.className = "text-6xl font-serif";
 
   const heroParagraph = document.createElement("p");
   heroParagraph.textContent =
     "The Tastiest & Best Pizza In The World. Have the real uniquely crafted experience.";
+  heroParagraph.className = "text-lg"
 
   const heroBtn = document.createElement("button");
   heroBtn.textContent = "View Menu";
   heroBtn.onclick = () => {
     console.log("Hero button clicked");
   };
+  heroBtn.className =
+    "w-fit bg-orange-500 text-white px-16 py-4 rounded-full font-bold shadow-xl shadow-orange-500/40 dark:shadow-orange-500/30";
 
-  heroLeft.className = "flex flex-col justify-center gap-4"
+  heroLeft.className = "flex flex-col justify-center gap-8 items-center md:items-start md:w-1/2";
   heroLeft.append(heroHeading, heroParagraph, heroBtn);
 
   const heroRight = document.createElement("div");
+  heroRight.className = "flex flex-col justify-center items-center";
+
+  const heroImgContainer = document.createElement("div");
+  heroImgContainer.className = "z-10 aspect-square max-h-full";
 
   const heroImg = new Image();
   heroImg.src = heroImage;
-  
+  heroImg.className = " filter drop-shadow-xl-dark";
 
-  const heroImgAfter = document.createElement("div")
-  heroImgAfter.className = "h-[100px] w-[250px] absolute bg-blue-400"
+  heroImgContainer.append(heroImg);
 
-  heroRight.append(heroImg, heroImgAfter);
+  // const heroImgAfter = document.createElement("div");
+  // heroImgAfter.className =
+  //   "h-[30vw] w-[35vw] right-0 mt-[15%] absolute bg-blue-400 rounded-l-full";
+
+  heroRight.append(heroImgContainer);
 
   heroSection.append(heroLeft, heroRight);
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
       sans: ['Open Sans', 'sans-serif'],
       serif: ['Philosopher', 'serif'],
     },
-    extend: {},
+    extend: {
+      dropShadow: {
+        'xl-dark': '0 35px 35px rgba(0, 0, 0, 0.35)',
+      }
+    },
   },
   variants: {
     extend: {},
